c6ea6da8e66cd73902dfc74215e0ba496029dfc5
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-06-05 21:47:38 +0200

    Added crawling - planning to scrap the project and make it with wikijs
    TODO: some fetching problems,
    wikipedia drops error 429 although I pass the header according to the documentation

--- public/client.js
+++ public/client.js
@@ -1,34 +1,65 @@
 "use strict"        // TODO - if not in wikipedia -> error | if no links -> error | if no input -> error
+let header = new Headers({ 'Api-User-Agent': 'WebCrawlerBOT3000/0.0 (email@gmail.com)', "User-Agent": "WebCrawlerBOT3000/0.0 (email@gmail.com)" })
 
 const submit = document.getElementById('submit');
 submit.addEventListener('click', async (event) => {
-    const start = document.getElementById('initialSubmition').value;
-    const search = await fetch(`/search/word/${start}`)
-        .catch(error => console.log(error));
+    let word = document.getElementById('initialSubmition').value;
+    word = word.split(" ").join("_");
+    const howDeep = document.getElementById('howDeep').value || 2;
+
+    const search = await fetch(`/search/word/${word}`, {
+        method: 'GET',
+        headers: header
+    }).catch(error => console.log(error));
     const searchJSON = await search.json();
-    console.log(searchJSON);
 
-    const keys = Object.keys(searchJSON.query.pages);
-    let key = keys.splice(Math.floor(Math.random() * keys.length), 1);
-    console.log(key);
-    while (typeof searchJSON.query.pages[key].extract === 'undefined') {
-        key = keys.splice(Math.floor(Math.random() * keys.length), 1);
-        if (keys.length === 0) {
-            console.log('No extracts'); // TODO smth when crawling finally is adedd
-            return;
-        }
+    let key = getKey(searchJSON.query.pages);
+
+    addtoPage(searchJSON.query.pages[key]);
+
+    for (let i = 0; i < howDeep; i++) {
+        const links = await fetch(`/links/key/${key}`, {
+            method: 'GET',
+            headers: header
+        }).catch(error => console.log(error));
+        const linksJSON = await links.json();
+
+        const keys = Object.keys(linksJSON.query.pages);
+        do {
+            key = keys[Math.floor(Math.random() * keys.length)];
+        } while (key < 0)
+
+        const search = await fetch(`/search/id/${key}`, {
+            method: 'GET',
+            headers: header
+        }).catch(error => console.log(error));
+        const searchJSON = await search.json();
+        console.log(searchJSON);
+        addtoPage(searchJSON.query.pages[key]);
     }
-    const title = searchJSON.query.pages[key].title;
-    const extract = searchJSON.query.pages[key].extract;
+})
 
+function addtoPage(page) {
     const root = document.createElement('div');
     const articleTitle = document.createElement('h4');
     const articleExtract = document.createElement('p');
 
-    articleTitle.textContent = title;
-    articleExtract.textContent = extract;
+    articleTitle.textContent = page.title;
+    articleExtract.textContent = page.extract;
 
     root.append(articleTitle, articleExtract);
     document.body.appendChild(root);
+}
 
-})
+function getKey(pages) {
+    const keys = Object.keys(pages);
+    let key = keys.splice(Math.floor(Math.random() * keys.length), 1);
+    while (typeof pages[key].extract === 'undefined') {
+        key = keys.splice(Math.floor(Math.random() * keys.length), 1);
+        if (keys.length === 0) {
+            console.log('No extracts'); // TODO 
+            return;
+        }
+    }
+    return key;
+}
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 "use strict"
 const express = require('express');
-const Datastore = require('nedb');
 const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,9 +8,36 @@ app.listen(PORT, () => console.log(`Listening at ${PORT}`));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/search/word/:word', async (request, response) => { // proxy to avoid CORS
+app.get('/search/word/:word', async (request, response) => { // proxy to avoid CORS GETS list of pages with the word in title and a short synopsis
     const word = request.params.word;
     const wikipediaURL = `https://en.wikipedia.org/w/api.php?action=query&generator=prefixsearch&gpssearch=${word}&prop=extracts&exintro=1&explaintext=1&redirects=1&format=json&gpslimit=30`;
+    const search = await fetch(wikipediaURL);
+    if (search.status >= 200 && search.status <= 299) {
+        console.log(wikipediaURL);
+        const searchJSON = await search.json();
+        response.json(searchJSON);
+    } else {
+        console.log(search.status, search.statusText);
+    }
+});
+
+app.get('/search/id/:id', async (request, response) => { // proxy to avoid CORS GETS list of pages with the word in title and a short synopsis
+    const id = request.params.id;
+    const wikipediaURL = `https://en.wikipedia.org/w/api.php?action=query&pageids=${id}&prop=extracts&exintro=1&explaintext=1&redirects=1&format=json`;
+    const search = await fetch(wikipediaURL);
+    if (search.status >= 200 && search.status <= 299) {
+        console.log(wikipediaURL);
+        const searchJSON = await search.json();
+        response.json(searchJSON);
+    } else {
+        console.log(search.status, search.statusText);
+    }
+});
+
+
+app.get('/links/key/:key', async (request, response) => { // proxy to avoid CORS GETS list of links on the wikipedia page 
+    const key = request.params.key;
+    const wikipediaURL = `https://en.wikipedia.org/w/api.php?action=query&format=json&pageids=${key}&generator=links&gpllimit=30`;
     const search = await fetch(wikipediaURL);
     if (search.status >= 200 && search.status <= 299) {
         console.log(wikipediaURL);
